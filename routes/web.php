888e43eea53ebd50a5100d5b8cd1211e1fe8acd0
Nischal <nischalshrestha800@gmail.com>
2024-08-17 19:32:19 +0545

    .

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Route as FacadesRoute;
 
+route::get('/', function () {
+    return view('welcome');
+});
 
 FacadesRoute::resource('products', ProductController::class);
